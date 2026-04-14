2785f8b4b429818b3ffd0df9b9ca8bdb56965c4c
Luis Gustavo <gamesso587@gmail.com>
2026-04-14 07:44:39 -0300

    Uncomment code for user input and greetings

--- ex01.js
+++ ex01.js
@@ -1,20 +1,20 @@
 // Atividade 1
 
-// const readline = require('readline-sync');
+const readline = require('readline-sync');
 
-// let nome = readline.question("Digite seu nome: ")
+let nome = readline.question("Digite seu nome: ")
 
-// if (nome == "Alice") {
-//     console.log("Olá, Alice! Bem-vinda de volta!")
-// } else {
-//     console.log(`Olá, ${nome}! Prazer em conhecê-lo!`)
-// }
+if (nome == "Alice") {
+console.log("Olá, Alice! Bem-vinda de volta!")
+} else {
+console.log(`Olá, ${nome}! Prazer em conhecê-lo!`)
+ }
 
 //Atividade 2
-// const readline = require('readline-sync');
+const readline = require('readline-sync');
 
-// let nome = readline.question("Digite seu nome: ")
-// let curso = readline.question("Qual seu curso: ")
-// let idade = readline.question("Qual sua idade: ")
+let nome = readline.question("Digite seu nome: ")
+let curso = readline.question("Qual seu curso: ")
+let idade = readline.question("Qual sua idade: ")
 
-// console.log(`Olá, ${nome}! Você tem ${idade} anos e está cursando ${curso}, Bem-vindo!`)
+console.log(`Olá, ${nome}! Você tem ${idade} anos e está cursando ${curso}, Bem-vindo!`)
